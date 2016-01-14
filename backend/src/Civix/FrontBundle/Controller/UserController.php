ccf3a20ee40c45a53236753cd3844dc615ee6a46
Ángel Guzmán Maeso <shakaran@gmail.com>
2016-01-14 17:13:35 +0100

    Implement more entry points, forgot password from secure api controller and others

--- backend/src/Civix/FrontBundle/Controller/UserController.php
+++ backend/src/Civix/FrontBundle/Controller/UserController.php
@@ -16,7 +16,7 @@ use Civix\CoreBundle\Entity\User;
 /**
  * User controller (frontend)
  *
- * @Route("/users")
+ * @Route("/")
  */
 class UserController extends Controller
 {
@@ -28,7 +28,7 @@ class UserController extends Controller
     public function indexAction()
     {
         if (true === $this->get('security.context')->isGranted('ROLE_USER')) {
-            return $this->redirect($this->generateUrl('civix_front_user_approvals'));
+            return $this->redirect($this->generateUrl('civix_front_user'));
         }
 
         return array();
@@ -55,4 +55,91 @@ class UserController extends Controller
                 'csrf_token' => $csrfToken
             ));
     }
+
+    /**
+     * @Route("/beta", name="civix_front_user_beta")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function betaAction()
+    {
+        if (true === $this->get('security.context')->isGranted('ROLE_USER')) {
+            return $this->redirect($this->generateUrl('civix_front_user'));
+        }
+
+        return array();
+    }
+	
+	/**
+     * @Route("/forgot-password", name="civix_front_user_forgot_password")
+     */
+    public function forgotPassword(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('CivixCoreBundle:User')->findOneBy(array(
+            'email' => $request->get('email')
+        ));
+        if (!$user) {
+            throw new \Symfony\Component\HttpKernel\Exception\HttpException(404);
+        }
+        
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        //check reset expiration
+        if (!$this->get('civix_core.user_manager')->checkResetInterval($user)) {
+            $response->setStatusCode(400)->setContent(json_encode(array('errors' =>
+                array(array(
+                    'message' => 'The password for this user has already been requested within the last 24 hours.'
+                    )
+                )))
+            );
+
+            return $response;
+        }
+
+        //Generate reset token, set date of reset and sent email
+        $resetPasswordToken = base_convert(bin2hex(hash('sha256', uniqid(mt_rand(), true), true)), 16, 36);
+        $user->setResetPasswordToken($resetPasswordToken);
+        $user->setResetPasswordAt(new \DateTime());
+        $em->persist($user);
+        $em->flush($user);
+        
+        //send mail
+        $this->get('civix_core.email_sender')->sendResetPasswordEmail(
+            $user->getEmail(),
+            array(
+                'name' => $user->getOfficialName(),
+                'link' => $this->getWebDomain() . '/#/reset-password/'. $resetPasswordToken
+            )
+        );
+        $response->setContent(json_encode(array('status'=>'ok')))->setStatusCode(200);
+
+        return $response;
+    }
+
+    /**
+     * @return string
+     */
+    private function getToken()
+    {
+        return $this->get('form.csrf_provider')->generateCsrfToken('superuser');
+    }
+
+    private function getWebDomain()
+    {
+        $request = $this->getRequest();
+        $host = $request->getHttpHost();
+
+        return $request->getScheme() . '://' .  str_replace('admin.', '', $host);
+    }
+	
+	/*
+	 /user/registration
+	/user/facebook/login
+	/user/registration-facebook
+	/user/forgot-password
+	/user/resettoken/
+	/user/beta
+	 */
 }
